42023b67533e601250ca209cfba02f306d4d96b4
unknown <jaquelinenunespontes@gmail.com>
2023-02-28 19:46:15 -0300

    https://github.com/jaquenunespontes/Blog-Pessoal_React.git

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import home from'./paginas/home/Home';
+import './App.css';
+import Home from './paginas/home/Home';
+
+
+
+function App() {
+  return (
+    <Home />
+    
+  );
+}
+
+export default App;
---
+++ src/paginas/home/Home.tsx
@@ -0,0 +1,14 @@
+import react from 'react'
+import './Home.css';
+
+function Home(){
+return (
+    <div>
+    <h1 className="titulo">Home</h1>
+    <img src="https://img.olhardigital.com.br/wp-content/uploads/2021/09/desenvolvedor-programador-TI.jpeg" 
+    alt="Imagem Tela Inicial" className="img" />
+    </div>
+);
+}
+
+export default Home;
