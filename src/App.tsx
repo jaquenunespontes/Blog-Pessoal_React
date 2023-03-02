e63eef9db0d664b3f620b67af295e096379484c9
unknown <jaquelinenunespontes@gmail.com>
2023-03-02 11:35:12 -0300

    react

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,27 @@
 import React from 'react';
-import home from'./paginas/home/Home';
-import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Navbar from './components/estaticos/navbar/Navbar';
+import Footer from './components/estaticos/footer/footer';
 import Home from './paginas/home/Home';
-
+import './App.css';
+import Login from './paginas/login/Login';
 
 
 function App() {
   return (
-    <Home />
-    
+    <Router>
+      <Navbar />
+      <Routes>
+
+        <Route path="/" element={<Home />} />
+
+        <Route path="/home" element={<Home />} />
+
+        <Route path="/login" element={<Login />} />
+      </Routes>
+      <Footer />
+    </Router>
+
   );
 }
 
--- src/paginas/home/Home.tsx
+++ src/paginas/home/Home.tsx
@@ -1,14 +1,31 @@
-import react from 'react'
+import React from 'react';
+import {Typography, Grid, Button} from '@material-ui/core';
+import {Box} from '@mui/material';
 import './Home.css';
 
-function Home(){
-return (
-    <div>
-    <h1 className="titulo">Home</h1>
-    <img src="https://img.olhardigital.com.br/wp-content/uploads/2021/09/desenvolvedor-programador-TI.jpeg" 
-    alt="Imagem Tela Inicial" className="img" />
-    </div>
-);
+function Home() {
+    return (
+        <>
+            <Grid container direction="row" justifyContent="center" alignItems="center" className='caixa'>
+                <Grid alignItems="center" item xs={6}>
+                    <Box paddingX={20} >
+                        <Typography variant="h3" gutterBottom color="textPrimary" component="h3" align="center" className='titulo'>Seja bem vindo(a)!</Typography>
+                        <Typography variant="h5" gutterBottom color="textPrimary" component="h5" align="center" className='titulo'>expresse aqui os seus pensamentos e opiniões!</Typography>
+                    </Box>
+                    <Box display="flex" justifyContent="center">
+                        <Box marginRight={1}>
+                        </Box>
+                        <Button variant="outlined" className='botao'>Ver Postagens</Button>
+                    </Box>
+                </Grid>
+                <Grid item xs={6} >
+                    <img src="https://services.meteored.com/img/article/nossa-compreensao-do-que-vemos-no-universo-pode-estar-errada-1675635379657_768.jpg" alt="" width="500px" height="400px" />
+                </Grid>
+                <Grid xs={12} className='postagens'>
+                </Grid>
+            </Grid>
+        </>
+    );
 }
 
 export default Home;
