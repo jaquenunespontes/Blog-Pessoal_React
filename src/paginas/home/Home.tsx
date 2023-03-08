161d1d4df6b7a46bcab57a82ffc7928d793616ee
unknown <jaquelinenunespontes@gmail.com>
2023-03-08 15:38:00 -0300

    atualizacao react

--- src/paginas/home/Home.tsx
+++ src/paginas/home/Home.tsx
@@ -1,9 +1,24 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import {Typography, Grid, Button} from '@material-ui/core';
 import {Box} from '@mui/material';
+import TabPostagem from '../../components/postagens/tabpostagem/TabPostagem';
+import ModalPostagem from '../../components/postagens/modalPostagem/ModalPostagem';
 import './Home.css';
+import { useNavigate } from 'react-router';
+import useLocalStorage from 'react-use-localstorage';
 
 function Home() {
+
+    let navigate = useNavigate();
+    const [token, setToken] = useLocalStorage('token');
+    
+    useEffect(() => {
+      if (token == "") {
+          alert("Você precisa estar logado")
+          navigate("/login")
+  
+      }
+  }, [token])
     return (
         <>
             <Grid container direction="row" justifyContent="center" alignItems="center" className='caixa'>
@@ -14,14 +29,16 @@ function Home() {
                     </Box>
                     <Box display="flex" justifyContent="center">
                         <Box marginRight={1}>
+                            <ModalPostagem />
                         </Box>
                         <Button variant="outlined" className='botao'>Ver Postagens</Button>
                     </Box>
                 </Grid>
                 <Grid item xs={6} >
-                    <img src="https://services.meteored.com/img/article/nossa-compreensao-do-que-vemos-no-universo-pode-estar-errada-1675635379657_768.jpg" alt="" width="500px" height="400px" />
+                    <img src="https://i.imgur.com/H88yIo2.png" alt="" width="500px" height="500px" />
                 </Grid>
                 <Grid xs={12} className='postagens'>
+                    <TabPostagem />
                 </Grid>
             </Grid>
         </>
